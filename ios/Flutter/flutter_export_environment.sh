990851bc0fc7650b762c0c844686fc4a228ee187
Andy Feidje Joseph <fedjee2007@yahoo.fr>
2020-06-05 16:49:19 -0400

    first commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/Andy_Softmatech/Flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/Andy_Softmatech/IdeaProjects/RestaurantAppUIKit"
+export "FLUTTER_TARGET=/Users/Andy_Softmatech/IdeaProjects/RestaurantAppUIKit/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/Andy_Softmatech/Flutter/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=5.0.0"
+export "FLUTTER_BUILD_NUMBER=50"
+export "TRACK_WIDGET_CREATION=true"
